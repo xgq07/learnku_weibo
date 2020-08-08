bd87bcc05125d2e935bf3f8631754fb303943924
xgq07 <303019754@qq.com>
2020-08-08 17:29:36 +0800

    增加 CSRF 验证

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 
-
+Route::resource('users', 'UsersController');
 
 
