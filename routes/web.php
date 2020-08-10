6b7699b6bf9452a0e0d981def2c2714ae9ae5f3d
qiu <goodgod2006@126.com>
2020-08-10 15:08:32 +0800

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
